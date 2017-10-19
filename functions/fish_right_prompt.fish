ab4ec55f4e1609d7554342fc3eb11decc0d93045
Elias Meire <me@eliasmei.re>
2017-10-19 13:28:23 +0200

    emojis!

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,13 +1,15 @@
 function fish_right_prompt
     set -l status_color cyan
+    set -l status_char "😄 "
 
     if test $status -ne 0
         set status_color red
+        set status_char "😡 "
     end
 
     if test "$CMD_DURATION" -gt 50
         set -l duration (printf $CMD_DURATION | humanize_duration)
-        print_color $status_color $duration
+        print_color $status_color "$status_char $duration"
     else
         print_color brblack (date "+%a %H:%M")
     end
