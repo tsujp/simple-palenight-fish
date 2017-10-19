8b6e970082106d8555f7659b6fa221067ea4ffb4
Elias Meire <me@eliasmei.re>
2017-10-19 13:43:25 +0200

    status

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -13,22 +13,25 @@ function fish_prompt -d "Simple fish prompt"
     # git
     if git_is_repo
         set -l git_char ""
-        set -l git_state "✓"
+        set -l ahead_char "||>"
+        set -l behind_char "<||"
+        set -l diverged_char "<=>"
+        set -l uptodate_char "✓"
         set -l branch_name (git_branch_name)
-        set -l git_ahead_char (git_ahead "||>" "<||" "<=>" "")
+        set -l git_ahead_char (git_ahead $ahead_char $behind_char $diverged_char $uptodate_char)
 
         # git branch
         printf " on "
         print_color blue "$git_char $branch_name"
 
-        # git state
+        # git status
+        set git_state $git_ahead_char
         if git_is_touched
             set git_state "++"
-        else if test -n $git_ahead_char
-            set git_state $git_ahead_char
         end
 
-        print_color green " $git_state"
+        printf " status "
+        print_color green $git_state
     end
 
     # new line
@@ -37,7 +40,7 @@ function fish_prompt -d "Simple fish prompt"
     # root indicator
     test (whoami) = root; and printf $superuser_char
 
-     # last command status
+    # last command status
     if test $prev_status -eq 0
         print_color yellow $command_char
     else
