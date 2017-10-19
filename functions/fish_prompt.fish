be207adb696c1b8070fa6c836ebd9da4ada9a430
Elias Meire <me@eliasmei.re>
2017-10-19 11:31:57 +0200

    fix prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -20,11 +20,11 @@ function fish_prompt -d "Palenight fish prompt"
     if git_is_repo
         set -l git_char ""
         set -l git_status "✓"
-        set -l branch_name git_branch_name
+        set -l branch_name (git_branch_name)
         set -l git_state (git_is_touched; and printf "++"; or git_ahead "||>" "<||" "<=>" "")
 
         printf " on "
-        print_color blue "($git_char$branch_name $git_state)"
+        print_color blue $git_char$branch_name $git_state
     end
 
     print_color red "\e[K\n❯ "
