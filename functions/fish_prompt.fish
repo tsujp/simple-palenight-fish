722de30d51c24762ef1e37abbf7060a0d4ea3cab
Elias Meire <me@eliasmei.re>
2017-10-19 19:50:13 +0200

    Change git status characters

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -12,17 +12,18 @@ function fish_prompt -d "Simple fish prompt"
 
     # git
     if git_is_repo
-        set -l ahead_char "||>"
-        set -l behind_char "<||"
-        set -l diverged_char "<=>"
+        set -l ahead_char "↑"
+        set -l behind_char "↓"
+        set -l diverged_char "↓↑"
         set -l uptodate_char "✓"
+        set -l touched_char "++"
         set -l branch_name (git_branch_name)
         set -l git_ahead_char (git_ahead $ahead_char $behind_char $diverged_char $uptodate_char)
 
         # set git state
         set git_state $git_ahead_char
         if git_is_touched
-            set git_state "++"
+            set git_state $touched_char
         end
 
         # print git branch + status
